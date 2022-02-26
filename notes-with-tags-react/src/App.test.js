105b6b4084bcb776eb55cc5978ee02106f9beb67
Ronald Lee <42512892+ronaldleepak@users.noreply.github.com>
2022-02-27 00:07:22 +0100

    Create React project

---
+++ notes-with-tags-react/src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders hello text', () => {
+  render(<App />);
+  const helloElement = screen.getByText(/hello/i);
+  expect(helloElement).toBeInTheDocument();
+});
