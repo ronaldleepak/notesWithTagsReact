3808285c77d2b71d5392ac51a5b8d94c29615bc4
Ronald Lee <42512892+ronaldleepak@users.noreply.github.com>
2022-02-27 20:11:26 +0100

    Add test for rendering all initial UI

--- notes-with-tags-react/src/App.test.js
+++ notes-with-tags-react/src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders hello text', () => {
-  render(<App />);
-  const helloElement = screen.getByText(/hello/i);
-  expect(helloElement).toBeInTheDocument();
+test('renders all initial UI', () => {
+    render(<App />);
+    const headerElement = screen.getByText(/Welcome to NotesWithTags!/i);
+    expect(headerElement).toBeInTheDocument();
+
+    const notesButtonsElement = screen.getByText(/New Note/i);
+    expect(notesButtonsElement).toBeInTheDocument();
+
+    const importElement = screen.getByText(/Import .NWT file/i);
+    expect(importElement).toBeInTheDocument();
+
+    const exportElement = screen.getByText(/Export .NWT file/i);
+    expect(exportElement).toBeInTheDocument();
 });
