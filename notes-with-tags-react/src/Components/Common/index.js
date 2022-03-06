d5e54f7afc966153f577eebc96caf097fc4a8755
Ronald Lee <42512892+ronaldleepak@users.noreply.github.com>
2022-03-06 14:38:10 +0100

    Add card to common component

--- notes-with-tags-react/src/Components/Common/index.js
+++ notes-with-tags-react/src/Components/Common/index.js
@@ -1,3 +1,4 @@
 export { default as ButtonGroup } from './ButtonGroup';
 export { default as NewLineText } from './NewLineText';
 export { default as CreatedModifiedDate } from './CreatedModifiedDate';
+export { default as Card } from './Card';
