23a7480b2ed4a60f7626b18c1d717127dc832644
Ronald Lee <42512892+ronaldleepak@users.noreply.github.com>
2022-03-06 19:21:00 +0100

    Create Textarea component

--- notes-with-tags-react/src/Components/Common/index.js
+++ notes-with-tags-react/src/Components/Common/index.js
@@ -1,4 +1,5 @@
 export { default as ButtonGroup } from './ButtonGroup';
 export { default as NewLineText } from './NewLineText';
+export { default as Textarea } from './Textarea';
 export { default as CreatedModifiedDate } from './CreatedModifiedDate';
 export { default as Card } from './Card';
