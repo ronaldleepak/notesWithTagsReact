8ce66259de56db7d94d12a4fdbc5fcd5f29e0cb3
Ronald Lee <42512892+ronaldleepak@users.noreply.github.com>
2022-03-16 21:45:47 +0100

    Add Authentication

--- notes-with-tags-react/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ notes-with-tags-react/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,5 +5,16 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "auth": {
+        "noteswithtagsreact55de89ef": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
     }
 }
--- notes-with-tags-react/src/App.js
+++ notes-with-tags-react/src/App.js
@@ -1,10 +1,15 @@
 import React from "react"
 import HomePage from "./Pages/Home";
-
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 export default class App extends React.Component {
     render() {
         return (
-            <HomePage/>
+                <Authenticator>
+                    {({ signOut, user }) => (
+                        <HomePage user={user}/>
+                    )}
+                </Authenticator>
         );
     }
 }
