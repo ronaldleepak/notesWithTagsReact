1a455600440ea5da53d82ac195e0617e455fcee2
Ronald Lee <42512892+ronaldleepak@users.noreply.github.com>
2022-04-02 20:50:25 +0200

    Add link button component

---
+++ src/Components/Common/LinkButton.js
@@ -0,0 +1,68 @@
+import React from "react"
+import PropTypes from 'prop-types'
+import { BUTTON_STYLE } from "../../Util/Constants"
+
+const {
+    NORMAL,
+    SUBMIT,
+    DANGER,
+} = BUTTON_STYLE;
+
+export default class LinkButton extends React.Component {
+    static propTypes = {
+        label: PropTypes.string,
+        name: PropTypes.string,
+        className: PropTypes.string,
+        href: PropTypes.string,
+        action: PropTypes.func,
+        buttonStyle: PropTypes.string,
+    }
+
+    buttonStyleToBulmaClass = (buttonStyle) => {
+        switch (buttonStyle) {
+            case NORMAL:
+                return "";
+            case SUBMIT:
+                return "is-success";
+            case DANGER:
+                return "is-danger";
+            default:
+                return "";
+        }
+    }
+
+    handleLinkButtonClick = (event) => {
+        const { action } = this.props;
+
+        event.stopPropagation();
+        event.preventDefault();
+
+        action(event);
+    }
+
+    render() {
+        var {
+            label,
+            name,
+            className,
+            href,
+            buttonStyle,
+        } = this.props;
+
+        const linkClassName = `is-light mr-4
+                        ${this.buttonStyleToBulmaClass(buttonStyle)}
+                        ${className}`;
+
+        return (
+            <div>
+                <a
+                    href={href}
+                    aria-label={name}
+                    className={linkClassName}
+                    onClick={this.handleLinkButtonClick}>
+                    {label}
+                </a>
+            </div>
+        );
+    };
+}
--- src/Components/Common/index.js
+++ src/Components/Common/index.js
@@ -1,4 +1,5 @@
 export { default as Button } from './Button';
+export { default as LinkButton } from './LinkButton';
 export { default as NewLineText } from './NewLineText';
 export { default as Textarea } from './Textarea';
 export { default as Textfield } from './Textfield';
