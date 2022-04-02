13516eb2d9d3948c8bbd11e37a9ff02689db399b
Ronald Lee <42512892+ronaldleepak@users.noreply.github.com>
2022-04-02 23:04:58 +0200

    Add user data to fetchUserData

--- src/Actions/FetchUserData.js
+++ src/Actions/FetchUserData.js
@@ -1,8 +1,13 @@
-import { fetchNoteList, fetchTagList } from "./";
+import {
+    fetchNoteList,
+    fetchTagList,
+    fetchCurrentUserData,
+} from "./";
 
 const fetchUserData = () => async (dispatch, getState) => {
     dispatch(fetchNoteList());
     dispatch(fetchTagList());
+    dispatch(fetchCurrentUserData());
 }
 
 export default fetchUserData;
