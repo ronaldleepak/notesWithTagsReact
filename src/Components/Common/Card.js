5ea29b0112c67374d092fa8fa5fa1349dfc8e0f5
Ronald Lee <42512892+ronaldleepak@users.noreply.github.com>
2022-04-22 23:34:19 +0200

    Fix children reseved word issue

--- src/Components/Common/Card.js
+++ src/Components/Common/Card.js
@@ -4,17 +4,13 @@ import PropTypes from 'prop-types'
 export default class Card extends React.Component {
     static propTypes = {
         name: PropTypes.string,
-        content: PropTypes.oneOfType([
-            PropTypes.object,
-            PropTypes.string,
-        ]),
     }
 
     render() {
-        var { content, name } = this.props;
+        var { children, name } = this.props;
         return (
             <div className="contents" role="listitem" aria-label={name}>
-                {content}
+                {children}
             </div>
         );
     };
