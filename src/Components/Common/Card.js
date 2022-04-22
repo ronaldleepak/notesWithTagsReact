200eafcad42c99319cc96bb818e5d455de4f145e
Ronald Lee <42512892+ronaldleepak@users.noreply.github.com>
2022-04-22 22:58:18 +0200

    Add proptypes for children prop

--- src/Components/Common/Card.js
+++ src/Components/Common/Card.js
@@ -4,6 +4,10 @@ import PropTypes from 'prop-types'
 export default class Card extends React.Component {
     static propTypes = {
         name: PropTypes.string,
+        children: PropTypes.oneOfType([
+            PropTypes.object,
+            PropTypes.string,
+        ]),
     }
 
     render() {
