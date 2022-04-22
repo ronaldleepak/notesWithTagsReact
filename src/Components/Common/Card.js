d35274b1294be6f13de34613961b2acc96b79fb0
Ronald Lee <42512892+ronaldleepak@users.noreply.github.com>
2022-04-22 22:58:47 +0200

    prop name change

--- src/Components/Common/Card.js
+++ src/Components/Common/Card.js
@@ -4,17 +4,17 @@ import PropTypes from 'prop-types'
 export default class Card extends React.Component {
     static propTypes = {
         name: PropTypes.string,
-        children: PropTypes.oneOfType([
+        content: PropTypes.oneOfType([
             PropTypes.object,
             PropTypes.string,
         ]),
     }
 
     render() {
-        var { children, name } = this.props;
+        var { content, name } = this.props;
         return (
             <div className="contents" role="listitem" aria-label={name}>
-                {children}
+                {content}
             </div>
         );
     };
